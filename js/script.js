76bd564ffc691e4b1e3747fc7d120ff20502fcfc
Juan Carlos Fuentes Lamas <juancarlos@siros.global>
2020-06-14 23:08:28 +0200

    commit for fetch

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import hamburguerMenu from "./menu_hamburguesa.js";
-import {digitalClock, alarm} from "./reloj.js";
+import { digitalClock, alarm } from "./reloj.js";
+import scrollTopButton from "./boton_scroll.js";
 
 const d = document;
 
@@ -8,4 +9,6 @@ d.addEventListener('DOMContentLoaded', e => {
     hamburguerMenu('.panel-btn', '.panel', '.menu a');
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm('../assests/alarma.mp3', '#activar-alarma', '#desactivar-alarma');
+
+    scrollTopButton(".scroll-top-btn");
 });
