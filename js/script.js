eeb33b8be78ca756f9e0b4c6a6ae802f68ab7141
Juan Carlos <jcfmalaga@gmail.com>
2020-06-14 11:31:30 +0200

    cuenta regresiva implemented

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 import hamburguerMenu from "./menu_hamburguesa.js";
 import {digitalClock, alarm} from "./reloj.js";
+import {shortcuts, moveBall} from "./teclado.js";
+import countdown from "./cuenta_regresiva.js";
 
 const d = document;
 
-d.addEventListener('DOMContentLoaded', e => {
+d.addEventListener('DOMContentLoaded', (e) => {
 
     hamburguerMenu('.panel-btn', '.panel', '.menu a');
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm('../assests/alarma.mp3', '#activar-alarma', '#desactivar-alarma');
+    countdown('countdown', "Dec 26, 2020 12:00:00", "Feliz Cumpleaños 🤓");
 });
+
+d.addEventListener('keydown', (e) => {
+    shortcuts(e);
+    moveBall(e, ".ball", ".stage");
+})
